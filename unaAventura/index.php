77cbb79a5891c63532e44175744ea55809788306
RebecaZ <49927491+RebecaZ@users.noreply.github.com>
2019-05-06 10:49:20 -0500

    Update index.php

--- unaAventura/index.php
+++ unaAventura/index.php
@@ -15,7 +15,7 @@ $result = mysqli_query($conect, $sql);
 	
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <title>hello</title>
+  <title>hola</title>
 	<link rel="stylesheet" href="fuentes/fuentes.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/estilos.css">
